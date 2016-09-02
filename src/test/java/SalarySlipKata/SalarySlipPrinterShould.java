6f3ad20ab500e5ec037961da26deff07e20d7bef
Mani Sarkar <sadhak001@gmail.com>
2016-09-02 14:24:01 +0100

    Replaced the SalarySlipService.java with the SalarySlipPrinter class. Renamed the test class reflect the implementation class SalarySlipPrinter.

--- src/test/java/SalarySlipKata/SalarySlipPrinterShould.java
+++ src/test/java/SalarySlipKata/SalarySlipPrinterShould.java
@@ -8,23 +8,23 @@ import static java.time.LocalDate.parse;
 import org.junit.Before;
 import org.junit.Test;
 
-import SalarySlipKata.application_service.SalarySlipService;
 import SalarySlipKata.domain.EmployeeId;
 import SalarySlipKata.domain.GBP;
 import SalarySlipKata.donain_service.EmployeeSalaryRepository;
 import SalarySlipKata.donain_service.SalaryService;
 import SalarySlipKata.infrastructure.Clock;
 import SalarySlipKata.infrastructure.Console;
+import SalarySlipKata.infrastructure.SalarySlipPrinter;
 
-public class SalarySlipServiceShould {
+public class SalarySlipPrinterShould {
   private static final EmployeeId EMPLOYEE_ID_12345 = new EmployeeId(12345);
 
   private Console console;
   private Clock clock;
 
-  private SalarySlipService salarySlipService;
   private SalaryService salaryService;
   private EmployeeSalaryRepository employeeSalaryRepository;
+  private SalarySlipPrinter salarySlipPrinter;
 
   @Before
   public void initialise() {
@@ -34,7 +34,7 @@ public class SalarySlipServiceShould {
     employeeSalaryRepository = new EmployeeSalaryRepository();
 
     salaryService = new SalaryService(employeeSalaryRepository);
-    salarySlipService = new SalarySlipService(console, clock, salaryService);
+    salarySlipPrinter = new SalarySlipPrinter(console, clock, salaryService);
   }
 
   @Test public void
@@ -42,7 +42,7 @@ public class SalarySlipServiceShould {
     when(clock.getCurrentDate()).thenReturn(parse("2016-09-01"));
     salaryService.addBasicSalaryFor(EMPLOYEE_ID_12345, new GBP(2000.00), parse("2016-09-01"));
 
-    salarySlipService.printSalaryFor(EMPLOYEE_ID_12345);
+    salarySlipPrinter.printSalaryFor(EMPLOYEE_ID_12345);
 
     verify(console).print("Date: 01 Sep 2016            Salary for period: Sep 2016\n");
     verify(console).print("\n");
